03ec2586b9fa2bd6cdbdf5f10fab3a23c469ca48
camilo177 <juanc.lopezca@campusucc.edu.co>
2024-05-03 09:02:40 -0500

    all views done

--- myProject/myApp/urls.py
+++ myProject/myApp/urls.py
@@ -10,10 +10,10 @@ from django.urls import path
 
 urlpatterns = [
     path('createInfo', CreateInfoView.as_view()),
-    path('updateInfo/id=<int:pk>', UpdateInfoView.as_view()),
+    path('updateInfo/id=<int:pk>', UpdateInfoView.as_view(), name='update_info'),
     path('readInfo/id=<int:pk>', ReadInfoView.as_view(), name='read_info'),
     path('deleteInfo/id=<int:pk>', DeleteInfoView.as_view(), name='delete_info'),
     path('createAdminProfile', CreateAdminProfileView.as_view()),
     path('createProduction', CreateProductionView.as_view()),
-    path('watchProduction/id=<int:pk>', WatchProductionView.as_view())
+    path('watchProduction/id=<int:pk>', WatchProductionView.as_view(), name='watch_production')
 ]
